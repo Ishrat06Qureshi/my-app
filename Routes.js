d00042b85d2d900a12eaf4039e720b1c542abf46
ISHRAT <45869750+Ishrat06Qureshi@users.noreply.github.com>
2019-06-12 06:30:56 -0700

    3rd commit

--- Routes.js
+++ Routes.js
@@ -1,162 +1,157 @@
 const express = require("express")
-const router= express.Router();
-const StudentRegistration = require("../backend/Models/StudentModel")
-const CourseRegistration = require("../backend/Models/CourseModel")
+const router = express.Router();
+const StudentRegistration = require("../backend/Models/StudentModel");
+const CourseRegistration = require("../backend/Models/CourseModel");
 
 
-router.post("/studentRegistration", ( req , res ) => {
- 
-   const { name , courseName , email , number , address  } = req.body
-    const data = {
-        name ,
-        courseName,
-        email,
-        number,
-        address,
-    }
-    StudentRegistration.findOne({email:req.body.email}).
-        then((user) => {
-            if (user){
-                
-                return res.status(400).send("user with this email already exist")  
-            }
-            else{
-                StudentRegistration.create(data).
-                then((user) => {
-               res.status(200).send("successfully registered")
-                    return
-                
-                })
-            }
-        }) 
+
+
+router.post("/studentRegistration", (req, res) => {
+
+    // const { name,  email, number, address } = req.body
+    // const data = {
+    //     name,
+    //     email,
+    //     number,
+    //     address,
+    // }
+    // StudentRegistration.findOne({ email: req.body.email }).
+    //     then((user) => {
+    //         if (user) {
+
+    //             return res.status(400).send("user with this email already exist")
+    //         }
+    //         else {
+    //             StudentRegistration.create(data).
+    //                 then((user) => {
+    //                     res.status(200).send("successfully registered")
+    //                     return
+
+    //                 })
+    //         }
+    //     })
+
+
+    console.log("hello world ")
+
+
 })
 
 
-router.get("/studentRegistration",( req,res) => {
-    
-    StudentRegistration.find({}).then(( data )=>{
-        if(data){
+router.get("/studentRegistration", (req, res) => {
+
+    StudentRegistration.find({}).then((data) => {
+        if (data) {
             res.status(200).send(data)
-         }
-         else{
-             res.status(404).send(json({Error:"no data found"}))
-         }
+        }
+        else {
+            res.status(404).send(json({ Error: "no data found" }))
+        }
     })
-    
-})   
+
+})
 
 
 
 
 
-router.post("/Course", ( req , res ) => {
-    
-   const {courseName , courseDetails } = req.body
+router.post("/Course", (req, res) => {
+
+    const { courseName, courseDetails } = req.body
     const data = {
-       courseName,
-       courseDetails,     
+        courseName,
+        courseDetails,
     }
 
-    CourseRegistration.findOne({courseName:req.body.courseName}).
+    CourseRegistration.findOne({ courseName: req.body.courseName }).
         then((course) => {
-            if (course){
-                return res.status(400).send("course with this name already exist") 
+            if (course) {
+                return res.status(400).send("course with this name already exist")
             }
-            else{
+            else {
                 CourseRegistration.create(data).
-                then((course) => {
-               res.status(200).send("successfully registered")
-                    return
-                
-                })
+                    then((course) => {
+                        res.status(200).send("successfully registered")
+                        return
+
+                    })
             }
         })
 })
 
+router.get("/Course", (req, res) => {
 
-
-router.get("/Course", ( req , res ) => {
+    CourseRegistration.find({}).select('courseName Registeredstudents').
+    populate("Registeredstudents", "name").
+    then( (courses) => {
+        return res.status(200).json(courses)
+    });
     
-    const  registeredStudents = []
-    const  registrationDetails = {}
-    CourseRegistration.find().then((Courses ) => {
-    
-    //   Courses.map(( course ) => {
-    //       StudentRegistration.find({_id :{ $in:[...course.Registeredstudents]} , "name":1 }).
-    //       then(( student ) => console.log( student) )
-       
-        //   registrationDetails[course] = [Students]
-        //   registeredStudents.push(registrationDetails)
+});
+router.post("/courseEnroll", (req, res) => {
 
-    //   })
-        // return res.status(200).send( registeredStudents )
+    const { studentId, courseId } = req.body
 
 
 
+    let courses = []
+    let students = []
+
+    //     StudentRegistration.findById(studentId).then((std) => {
+
+    //         if(std){
+
+    //             CourseRegistration.findById(courseId).then((crs) => {
+
+    //                 if(crs){
+
+    //                     std.Registeredcourses.push(courseId);
+    //                     crs.Registeredstudents.push(studentId);
+
+    //                     StudentRegistration.findByIdAndUpdate(studentId,std).then( (stdReg) => {
+
+    //                         console.log("Student Registration");
+    //                         console.log(stdReg)
+    //                     } );
+
+    //                     CourseRegistration.findByIdAndUpdate(courseId,crs).then((crsReg) => {
+    //                         console.log("Course Registration");
+    //                         console.log(crsReg)   
+    //                     })
+    //                 }
+    //             })
+    //         }
+    //     })
+
+    // })
+
+    StudentRegistration.findById(studentId).then((student) => {
+
+        const { Registeredcourses } = student
+
+        courses = [...student.Registeredcourses, courseId]
+        if (Registeredcourses.includes(courseId)) {
+            return res.status(400).send("already register in this course")
+        }
+        StudentRegistration.findByIdAndUpdate(studentId, { Registeredcourses: courses })
+            .then(() => {
+
+
+                CourseRegistration.findById(courseId).then((course) => {
+
+                    students = [...course.Registeredstudents, studentId]
+
+                    CourseRegistration.findByIdAndUpdate(courseId, { Registeredstudents: students }).
+                        then(() => {
+                            return res.status(200).send("suceessfully register")
+
+                        })
+                })
+            })
     })
 
-})
 
 
-
-
-
-router.post("/courseEnroll", ( req , res ) => {
-    
-    const {studentId , courseId } = req.body
-
-    console.log(studentId);
-    console.log(courseId);
-
-    let  courses = []
-    let  students = []
-
-//     StudentRegistration.findById(studentId).then((std) => {
-
-//         if(std){
-
-//             CourseRegistration.findById(courseId).then((crs) => {
-
-//                 if(crs){
-
-//                     std.Registeredcourses.push(courseId);
-//                     crs.Registeredstudents.push(studentId);
-
-//                     StudentRegistration.findByIdAndUpdate(studentId,std).then( (stdReg) => {
-
-//                         console.log("Student Registration");
-//                         console.log(stdReg)
-//                     } );
-
-//                     CourseRegistration.findByIdAndUpdate(courseId,crs).then((crsReg) => {
-//                         console.log("Course Registration");
-//                         console.log(crsReg)   
-//                     })
-//                 }
-//             })
-//         }
-//     })
-
-// })
-
-         StudentRegistration.findById(studentId).then(( student ) => {
-
-              courses=[...student.Registeredcourses,courseId] 
-
-              StudentRegistration.findByIdAndUpdate(studentId , {Registeredcourses: courses })
-              .then(() => console.log("its done "))
-         })
-
-
-         CourseRegistration.findById(courseId).then(( course ) => {
-            students=[...course.Registeredstudents,studentId]
-
-            CourseRegistration.findByIdAndUpdate(courseId , { Registeredstudents:students }).
-            then(() => res.status(200).send("done ")) 
-        
-         
-
-})
 
 })
 
